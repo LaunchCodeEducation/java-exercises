967d36d020206cf7354e019fcbff97596d1bf66a
paul <paulvrobson@gmail.com>
2017-11-02 18:57:57 -0500

    Add negative radius validation

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -6,12 +6,19 @@ public class Area {
 
     private static final double PI = 3.14;
 
+    private static boolean isNotValidRadius(double radius) {
+        return (radius < 0);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a radius:");
         double radius = scanner.nextDouble();
-        double area = PI * Math.pow(radius, 2);
-        System.out.println("The area of a circle of radius " + radius + " is: " + area);
-
+        if (isNotValidRadius(radius)) {
+            System.out.println("Not a valid radius!");
+        } else {
+            double area = PI * Math.pow(radius, 2);
+            System.out.println("The area of a circle of radius " + radius + " is: " + area);
+        }
     }
 }
