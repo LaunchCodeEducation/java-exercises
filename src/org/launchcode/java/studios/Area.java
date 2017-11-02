b20a87e8e27d1b0113f7a492740146ebced47d76
paul <paulvrobson@gmail.com>
2017-11-02 18:44:01 -0500

    Implement area algorithm

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -1,4 +1,17 @@
 package org.launchcode.java.studios;
 
+import java.util.Scanner;
+
 public class Area {
+
+    private static final double PI = 3.14;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a radius:");
+        double radius = scanner.nextDouble();
+        double area = PI * Math.pow(radius, 2);
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+
+    }
 }
