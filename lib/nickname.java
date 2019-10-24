295070797a14168f6ebe20590b2e981f43a35dcb
melissafalcao <32322512+melissafalcao@users.noreply.github.com>
2019-10-24 00:07:52 -0300

    Create nickname.java

---
+++ lib/nickname.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class HuxleyCode {
+  public static void main(String args[]) {
+      
+      String N, S;
+		
+		Scanner in = new Scanner(System.in);
+		N = in.nextLine();
+		S = in.nextLine();
+		
+		System.out.print(N + S);
+  }
+}
